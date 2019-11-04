0c24ba3ccc3055ac1ad4e67de118d3015747230f
simone celia <simon387@hotmail.it>
2019-11-04 13:08:25 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+var config = {
+  'checkipUrl': 'http://checkip.amazonaws.com/',
+  'checkipTag': 'pre',
+  'proxy': '',
+  'url': 'https://cp.tophost.it/',
+  'httpAuthUser': '',
+  'httpAuthPass': '',
+  'editDNSElement': 'body > table:nth-child(4) > tbody > tr:nth-child(1) > td:nth-child(4) > p:nth-child(2) > table:nth-child(2) > tbody > tr:nth-child(2) > td:nth-child(4) > a',
+  'editRecordElement': '#div-main > table > tbody > tr > td:nth-child(2) > table:nth-child(3) > tbody > tr:nth-child(8) > td:nth-child(5) > a',
+  'recordInputBox': '#td-label2 > input',
+}
